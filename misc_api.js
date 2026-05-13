3e440c023a8c8c7abf2e3cca1db2b11ca627d327
CheckerT <tom.tb.brand@gmx.de>
2026-05-13 09:01:51 +0200

    feat: added laptop/phone clipboard sync (push directly from laptop)

--- misc_api.js
+++ misc_api.js
@@ -1,5 +1,7 @@
 import { runCommand } from './helper_funcs.js';
 import { homework, saveHomework, Task } from './homework_manager.js';
+let clipboard = '';
+let clipboardSentFromPhone = false;
 
 export function setupMiscApi(app) {
   app.get('/api/code-start', (req, res) => {
@@ -14,11 +16,11 @@ export function setupMiscApi(app) {
     res.status(200).json('Code gestoppt');
   });
 
-  app.get("/api/homework", (req, res) => {
+  app.get('/api/homework', (req, res) => {
     res.json(homework);
   });
 
-  app.post("/api/homework", (req, res) => {
+  app.post('/api/homework', (req, res) => {
     const { name, description, dueDate, subject } = req.body;
     try {
       const task = new Task({
@@ -38,7 +40,7 @@ export function setupMiscApi(app) {
     }
   });
 
-  app.put("/api/homework/:id", (req, res) => {
+  app.put('/api/homework/:id', (req, res) => {
     const { id } = req.params;
     const task = homework.find(t => t.id === id);
     console.log(`Bearbeite ${task.name}`)
@@ -55,7 +57,7 @@ export function setupMiscApi(app) {
     res.json(task);
   });
 
-  app.post("/api/homework/:id/complete", (req, res) => {
+  app.post('/api/homework/:id/complete', (req, res) => {
     const { id } = req.params;
     const task = homework.find(t => t.id === id);
     console.log(`Erledige ${task.name}`)
@@ -66,12 +68,42 @@ export function setupMiscApi(app) {
     res.json(task);
   });
 
-  app.delete("/api/homework/:id", (req, res) => {
+  app.delete('api/homework/:id', (req, res) => {
     const index = homework.findIndex(t => t.id === req.params.id);
     console.log(`Index: ${index}`)
     if (index === -1) return res.status(404).json({ error: "Nicht gefunden" });
     homework.splice(index, 1);
     saveHomework(homework);
     res.json({ success: true });
+  });
+
+  app.get('/api/clipboard', async (req, res) => {
+    clipboardSentFromPhone = false
+try {
+        await fetch('https://ntfy.sh/ClipboardGetFromPhone_2147483647', { method: 'POST' });
+    } catch (err) {
+        console.error("ntfy Fehler:", err);
+    }
+    
+    let attempts = 0;
+    const interval = setInterval(() => {
+        attempts++;
+
+        if (clipboardSentFromPhone) {
+            clearInterval(interval);
+            return res.send(clipboard);
+        }
+
+        if (attempts > 50) { 
+            clearInterval(interval);
+            return res.status(408).send("Timeout: Handy hat nicht geliefert.");
+        }
+    }, 100);
+  });
+
+  app.post('/api/clipboard', (req, res) => {
+    clipboard = req.body.text;
+    clipboardSentFromPhone = true;
+    res.sendStatus(200);
   });
 }
