bade6dbc6de53a840589f771548884b67a2ee02a
CheckerT <tom.tb.brand@gmx.de>
2026-06-13 17:42:02 +0200

    feat: added chaining voice commands

--- smart_home_api.js
+++ smart_home_api.js
@@ -5,27 +5,37 @@ export function setupSmartHomeApi(app) {
     boxRequest(`main/setPower?power=${req.params.state}`);
     res.status(200).json('OK');
   });
-  
+
   app.get('/api/box/volume/:change', (req, res) => {
     const linkVar = req.params.change === 'up' ? 'up&step=2' : 'down&step=2';
     boxRequest(`main/setVolume?volume=${linkVar}`);
     res.status(200).json('OK');
   });
-  
+
   app.get('/api/box/input/:value', (req, res) => {
     const linkVar = req.params.value;
     boxRequest(`main/setInput?input=${linkVar}`);
     res.status(200).json('OK');
   });
-  
+
   app.post('/api/jarvis/', async (req, res) => {
     if (!req.body) return res.status(400).json('Body ist leer');
     let { body } = req.body;
     if (!body) return res.status(200).json('Du hast nichts gesagt, oder?');
     body = body.split(',')[0];
-  
+
     let input = body.replace(/[.,!?]/gi, '');
-    const result = await handleSpeech(input);
+    let result = '';
+
+    const segments = input.split(' und ');
+
+    // foreach blocked nicht => for of
+    for (const segment of segments) {
+      result += await handleSpeech(segment);
+      result += ' und ';
+    }
+
+    result = result.replace(/und\s*$/, '').trimEnd();
 
     console.log(`Result: ${result}, input: ${input}`);
     res.status(200).json(result);
